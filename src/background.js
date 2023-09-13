0c79b0f07eab36d7b78360a2ba32015aa9eea18c
Dec_32 <1064103102@qq.com>
2023-09-14 02:40:29 +0800

    Add support for chess.com homepage

--- src/background.js
+++ src/background.js
@@ -9,7 +9,10 @@ const sites = [
     },
     {
         url: "*://www.chess.com/*",
-        css: (color, piece, url) => `.${color[0]}${piece=="knight"?'n':piece[0]} {background-image: url(${url})!important}`
+        css: (color, piece, url) => [
+            `.${color[0]}${piece=="knight"?'n':piece[0]} {background-image: url(${url})!important}`,
+            `:root {--theme-piece-set-${color[0]}${piece=="knight"?'n':piece[0]}: url(${url})!important}`
+        ]
     }
 ]
 
@@ -42,15 +45,20 @@ function apply(color, piece, url, active=null) {
     revert(color, piece)
     console.debug(`Now customizing ${color} ${piece}`)
     for (let site of sites) {
-        let css = site.css(color, piece, url)  
+        let cssArr = site.css(color, piece, url)
+        if (!Array.isArray(cssArr)) {
+            cssArr = [cssArr]
+        }
         browser.tabs.query({url:site.url, active:active}, tabs => {    
             for (let tab of tabs) {
                 let target = {tabId: tab.id}
                 console.debug(`Inject CSS for ${color} ${piece} into tab #${tab.id} from "${site.url}"."`)
-                browser.scripting.insertCSS({target: target, css: css}, ()=>{
-                    // store the css so that it can be reverted
-                    putInjected(site, color, piece, css)
-                })
+                for (let css of cssArr) {
+                    browser.scripting.insertCSS({target: target, css: css}, ()=>{
+                        // store the css so that it can be reverted
+                        putInjected(site, color, piece, css)
+                    })
+                }
             }
         })
     }
@@ -58,9 +66,9 @@ function apply(color, piece, url, active=null) {
 
 function revert(color, piece) {
     for (let site of sites) {
-        let css = delInjected(site, color, piece)
+        let cssArr = delInjected(site, color, piece)
         browser.tabs.query({url:site.url}, tabs => {
-            if (!css) {
+            if (!cssArr) {
                 console.warn(`Can not find the injected CSS for ${color} ${piece} on "${site.url}" thus reverting would be aborted.`)
                 console.warn(injection)
                 return
@@ -69,7 +77,9 @@ function revert(color, piece) {
                 let target = {tabId: tab.id}
                 // store the css so that it can be reverted
                 console.debug(`Remove CSS for ${color} ${piece} from tab #${tab.id} from "${site.url}".`)
-                browser.scripting.removeCSS({target: target, css: css})
+                for (let css of cssArr) {
+                    browser.scripting.removeCSS({target: target, css: css})
+                }
             }
         })
 
@@ -80,15 +90,20 @@ function revert(color, piece) {
 // todo: optimize key length
 function putInjected(engine, color, piece, css) {
     let key = `${engine.url}-${color}-${piece}`
-    injection[key] = css
+    let arr = injection[key];
+    if (!arr) {
+        arr = []
+        injection[key] = arr
+    }
+    arr.push[css]
 }
 
 function delInjected(engine, color, piece) {
     let key = `${engine.url}-${color}-${piece}`
-    let css = injection[key]
-    if (css) {
+    let cssArr = injection[key]
+    if (cssArr) {
         injection.delete(key)
-        return css
+        return cssArr
     } else {
         return null
     }
