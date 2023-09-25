9d8a766dd70769fcfe0e91e8121dd60d32976fdc
dec32 <1064103102@qq.com>
2023-09-26 02:18:26 +0800

    Add full support for chess.com homepage

--- src/background.js
+++ src/background.js
@@ -4,21 +4,32 @@ import storage from "./storage.js"
 const sites = [
     {
         url: "*://lichess.org/*",
-        css: (color, piece, url) => `.is2d .${color}.${piece} {background-image: url(${url})!important}`  
+        css: (color, piece, url, tab) => `.is2d .${color}.${piece} {background-image: url(${url})!important}`  
     },
     {
         url: "*://www.chess.com/*",
-        css: (color, piece, url) => {
-            let id = `${color[0]}${piece=="knight"?'n':piece[0]}`
-            if (url.startsWith("data:")) {
-                return `.${id}{background-image: url(${url})!important}`
-                // or else it will break the homepage
-                // because each of the promo panels on the homepage uses one big background property to present the board
-                // if using data urls, those big background property strings will be so long that the CSS engine refuses to render thems
-            } else {
+        css: async function (color, piece, url, tab) {
+            if (tab.url.endsWith("chess.com/home")) {
+                let id = `${color[0]}${piece=="knight"?'n':piece[0]}`
+                if (url.startsWith("data:")) {
+                    // convert all data URLs blob URLs for the homepage
+                    // fixme: it flickers
+                    let results = await browser.scripting.executeScript({ target: { tabId: tab.id }, func: getBlobURL, args: [url] })
+                    if (results.find(result => result.error)) {
+                        console.error("Error detected in injected scripts.")
+                        console.error(results)
+                    } else {
+                        url = results[0].result
+                        console.debug(`Data URL converted to '${url}'`)
+                    }
+                }
                 return `:root {--theme-piece-set-${id}: url(${url})!important}`
+            } else {
+                return `.${id}{background-image: url(${url})!important}`
             }
         }
+
+
     }
 ]
 
@@ -50,13 +61,13 @@ async function apply(color, piece, url, tabId) {
         await revert(color, piece)
     }
     for (let site of sites) {
-        let css = site.css(color, piece, url)
         let tabs = await browser.tabs.query({url:site.url})
         for (let tab of tabs) {
             if (tabId && tabId != tab.id) {
                 continue;
             }
             let target = {tabId: tab.id}
+            let css = await site.css(color, piece, url, tab)
             await browser.scripting.insertCSS({ target: target, css: css })
             console.debug(`Tab #${tab.id}(${site.url}): Injected CSS for ${color} ${piece}."`)
             // store the css so that it can be reverted
@@ -98,4 +109,17 @@ function delInjected(site, color, piece) {
     let css = site.injected[key]
     site.injected.delete(key)
     return css
+}
+
+function getBlobURL(dataURL) {
+    let arr = dataURL.split(',')
+    let mime = arr[0].match(/:(.*?);/)[1]
+    let bstr = atob(arr[1])
+    let n = bstr.length
+    let u8arr = new Uint8Array(n);
+    while(n--){
+        u8arr[n] = bstr.charCodeAt(n);
+    }
+    let blob = new Blob([u8arr], {type:mime});
+    return URL.createObjectURL(blob);
 }
