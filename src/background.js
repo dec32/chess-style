239b68cdbf5ac1acf92dbd1e332e2184ffb27976
dec32 <1064103102@qq.com>
2023-09-26 02:21:55 +0800

    Added a content script for chess.com homepage to fix the flickering (reason unknown) but now CSS injection for the homepage will be delayed a bit.

--- src/background.js
+++ src/background.js
@@ -4,32 +4,14 @@ import storage from "./storage.js"
 const sites = [
     {
         url: "*://lichess.org/*",
-        css: (color, piece, url, tab) => `.is2d .${color}.${piece} {background-image: url(${url})!important}`  
+        css: (color, piece, url) => `.is2d .${color}.${piece} {background-image: url(${url})!important}`  
     },
     {
         url: "*://www.chess.com/*",
-        css: async function (color, piece, url, tab) {
-            if (tab.url.endsWith("chess.com/home")) {
-                let id = `${color[0]}${piece=="knight"?'n':piece[0]}`
-                if (url.startsWith("data:")) {
-                    // convert all data URLs blob URLs for the homepage
-                    // fixme: it flickers
-                    let results = await browser.scripting.executeScript({ target: { tabId: tab.id }, func: getBlobURL, args: [url] })
-                    if (results.find(result => result.error)) {
-                        console.error("Error detected in injected scripts.")
-                        console.error(results)
-                    } else {
-                        url = results[0].result
-                        console.debug(`Data URL converted to '${url}'`)
-                    }
-                }
-                return `:root {--theme-piece-set-${id}: url(${url})!important}`
-            } else {
-                return `.${id}{background-image: url(${url})!important}`
-            }
+        css: (color, piece, url) => {
+            let id = `${color[0]}${piece=="knight"?'n':piece[0]}`
+            return `.${id}{background-image: url(${url})!important}`
         }
-
-
     }
 ]
 
@@ -61,14 +43,14 @@ async function apply(color, piece, url, tabId) {
         await revert(color, piece)
     }
     for (let site of sites) {
+        let css = site.css(color, piece, url)
         let tabs = await browser.tabs.query({url:site.url})
         for (let tab of tabs) {
             if (tabId && tabId != tab.id) {
                 continue;
             }
             let target = {tabId: tab.id}
-            let css = await site.css(color, piece, url, tab)
-            await browser.scripting.insertCSS({ target: target, css: css })
+            browser.scripting.insertCSS({ target: target, css: css })
             console.debug(`Tab #${tab.id}(${site.url}): Injected CSS for ${color} ${piece}."`)
             // store the css so that it can be reverted
             putInjected(site, color, piece, css)
---
+++ src/chess.com.js
@@ -0,0 +1,38 @@
+// fixing for homepage
+const keys = []
+for (let color of ["black", "white"]) {
+    for (let piece of ["king", "queen", "rook", "bishop", "knight", "pawn"]) {
+        keys.push(`${color}_${piece}`)
+    }
+}
+
+// todo don't use the chrome key word
+let obj = chrome.storage.local.get(keys, obj=>{
+    for (let key of Object.keys(obj)) {
+        let split = key.split("_")
+        inject(split[0], split[1], obj[key])
+    }
+})
+
+const root = document.querySelector(':root').style
+
+function inject(color, piece, url) {
+    let id = `${color[0]}${piece=="knight"?'n':piece[0]}`
+    if (url.startsWith("data:")) {
+        url = getBlobURL(url)
+    }
+    root.setProperty(`--theme-piece-set-${id}`, `url(${url})`)
+}
+
+function getBlobURL(dataURL) {
+    let arr = dataURL.split(',')
+    let mime = arr[0].match(/:(.*?);/)[1]
+    let bstr = atob(arr[1])
+    let n = bstr.length
+    let u8arr = new Uint8Array(n);
+    while(n--){
+        u8arr[n] = bstr.charCodeAt(n);
+    }
+    let blob = new Blob([u8arr], {type:mime});
+    return URL.createObjectURL(blob);
+}
